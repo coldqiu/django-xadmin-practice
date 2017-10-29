3990cec70f9c472844b20a4a8cd587acf57c6898
coldqiu <30189716+coldqiu@users.noreply.github.com>
2017-10-29 09:03:03 +0800

    Add files via upload

---
+++ ColorOnline/urls.py
@@ -0,0 +1,63 @@
+# _*_ encoding:utf-8 _*_
+"""ColorOnline URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.views.static import serve
+import xadmin
+
+from users.views import LoginView, RegisterView, ActiveUserView, ForgetPwdView, ResetView, ModifyPwdView
+from users.views import LogoutView, IndexView
+from organization.views import OrgView
+from ColorOnline.settings import MEDIA_ROOT, STATIC_ROOT
+
+
+urlpatterns = [
+    url(r'^xadmin/', xadmin.site.urls),
+    url(r'^index$', IndexView.as_view(), name="index"),
+    url(r'^login/$', LoginView.as_view(), name="login"),
+    url(r'^logout/$', LogoutView.as_view(), name="logout"),
+
+    url(r'^register/$', RegisterView.as_view(), name="register"),
+    url(r'^captcha/', include('captcha.urls')),
+    url(r'^active/(?P<active_code>.*)/$', ActiveUserView.as_view(), name="user_active"),
+    url(r'^forget/$', ForgetPwdView.as_view(), name="forget_pwd"),
+    url(r'^reset/(?P<active_code>.*)/$', ResetView.as_view(), name="reset_pwd"),
+    url(r'^modify_pwd/$', ModifyPwdView.as_view(), name="modify_pwd"),
+    #配置上传文件的处理函数
+    url(r'^media/(?P<path>.*)$', serve, {"document_root":MEDIA_ROOT}),
+
+    #课程机构url配置
+    url(r'^org/', include('organization.urls', namespace="org")),
+
+    #课程相关url配置
+    url(r'^course/', include('courses.urls', namespace="course")),
+
+    #个人中心URL配置
+    url(r'^users/', include('users.urls', namespace="users")),
+
+
+    url(r'^static/(?P<path>.*)$', serve, {"document_root":STATIC_ROOT}),
+
+
+
+]
+
+
+handler404 = 'users.views.page_not_found'
+handler500 = 'users.views.page_not_found'
